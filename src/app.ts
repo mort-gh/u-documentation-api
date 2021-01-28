58ea196bacecb1da9b85403d08e11e5c4de6aa80
Roman Gnatyuk <54001169+mort-gh@users.noreply.github.com>
2021-01-28 17:23:51 +0200

    feat: add and connect swagger

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
-    logger: ['warn', 'error'],
+    logger: console,
   });
   app.useGlobalPipes(new ValidationPipe());
 
+  const config = new DocumentBuilder()
+    .setTitle('u-documentation-api')
+    .setDescription('u-documentation-api API description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3434, '0.0.0.0');
-  console.log(`Application is running on ${await app.getUrl()}`);
+  Logger.log(`Application is running on ${await app.getUrl()}`);
 }
 
 bootstrap();
